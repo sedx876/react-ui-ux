76b3bb8759d3fb7b8a5df8425f2181fd07c7cc09
sedx876 <swatkins876@gmail.com>
2022-01-28 09:24:40 -0500

    navbar header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import { CTA, Brand, Navbar } from './components'
 const App = () => {
   return (
     <div className="App">
-      Hello
+      <div className='gradient_bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand />
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
